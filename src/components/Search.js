ca7856e449e1707b33e29e4e602d02f13ef7a327
Rajat <rajatkr125@gmail.com>
2021-07-06 11:53:19 +0530

    Debounced term

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,8 +1,20 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+
 const Search = () => {
   const [term, setTerm] = useState('programming');
   const [results, setResults] = useState([]);
+  const [debouncedTerm, setDebouncedTerm] = useState(term);
+
+  useEffect(() => {
+    const timerId = setTimeout(() => {
+      setDebouncedTerm(term);
+    }, 1000);
+    return () => {
+      clearTimeout(timerId);
+    };
+  }, [term]);
+
   useEffect(() => {
     const search = async () => {
       const { data } = await axios.get('https://en.wikipedia.org/w/api.php', {
@@ -11,27 +23,14 @@ const Search = () => {
           list: 'search',
           origin: '*',
           format: 'json',
-          srsearch: term,
+          srsearch: debouncedTerm,
         },
       });
       setResults(data.query.search);
     };
+    search();
+  }, [debouncedTerm]);
 
-    if (term && !results.length) {
-      search();
-    } else {
-
-      const timeoutId = setTimeout(() => {
-        if (term) {
-          search();
-        }
-      }, 1000);
-      return () => {
-        clearTimeout(timeoutId);
-      };
-      
-    }
-  }, [term]);
   const renderedResults = results.map((result) => {
     return (
       <div key={result.pageid} className='item'>
